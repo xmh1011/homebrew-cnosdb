ffd2d96784767fa9f8d69ccf2c6bb1ee652636f2
Subsegment <74497693+Subsegment@users.noreply.github.com>
2022-06-02 10:39:48 +0800

    Create cnosdb.rb

---
+++ cnosdb.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Cnosdb < Formula
+  desc "An Open Source Distributed Time Series Database with high performance, high compression ratio and high usability."
+  homepage "https://www.cnosdb.com"
+  version "1.0.2"
+  url "https://github.com/cnosdb/cnosdb/releases/download/v1.0.2/cnosdb-1.0.2_darwin_amd64.tar.gz"
+  sha256 "c5b56f7a8f232518279dda4d8ef3486c47ed8bf3ca4e127046311eb81060bb3a"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cnosdb`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
